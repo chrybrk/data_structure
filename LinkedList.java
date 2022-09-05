49f0e42632d44d5a79fa37e69cbc5bcf41c2d8f7
chrybrk <rahul003chry@gmail.com>
2022-09-05 10:52:20 +0530

    linked_list

--- LinkedList.java
+++ LinkedList.java
@@ -3,6 +3,7 @@ import java.io.*;
 public class LinkedList
 {
     Node head;
+    int size = 0;
 
     static class Node
     {
@@ -27,6 +28,53 @@ public class LinkedList
             next_node.next = n;
         }
 
+        list.size++;
+
+        return list;
+    }
+
+    public static void print_by_index(LinkedList list, int index)
+    {
+        if (index >= list.size || index < 0) System.out.println("** OUT OF RANGE **");
+        else
+        {
+            Node next_node = list.head;
+
+            int i = 1;
+            while (i <= index)
+            {
+                next_node = next_node.next;
+                i++;
+            }
+
+            System.out.println(next_node.data + " ");
+        }
+    }
+
+    public static LinkedList delete_by_index(LinkedList list, int index)
+    {
+        Node n = list.head, pn = null;
+
+        if (index >= list.size || index < 0) System.out.println("** OUT OF RANGE **");
+
+        if (index == 0 && n != null)
+        {
+            list.head = n.next;
+            list.size--;
+        }
+        else
+        {
+            int i = 1;
+            while (i <= index && n != null)
+            {
+                pn = n; n = n.next;
+                i++;
+            }
+
+            if (n != null) pn.next = n.next;
+            list.size--;
+        }
+
         return list;
     }
 
@@ -44,5 +92,7 @@ public class LinkedList
             next_node = next_node.next;
         }
 
+        delete_by_index(list, 0);
+        print_by_index(list, 0);
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+- [x] LinkedList `java`
